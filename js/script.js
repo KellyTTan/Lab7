cde1ed368a14155768e4df6b32f780ea830b3fdd
KellyTTan <60084565+KellyTTan@users.noreply.github.com>
2020-04-14 19:16:47 -1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
+ function myEventFunction() {
+  alert('Kelly Tan: ');
+ }
+ var d2 = document.getElementById('entrybutton');
+ d2.onclick = myEventFunction;
 
 
 
