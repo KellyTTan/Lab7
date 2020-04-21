dfada44db723318b3731c3d817a56bb9a4bc303b
KellyTTan <60084565+KellyTTan@users.noreply.github.com>
2020-04-21 00:02:53 -1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function init(){
-    var name = document.getElementByClassName('center');
     var input = document.getElementById('entryinput');
     var output = document.getElementById('textoutput');
     var button = document.getElementById('entrybutton');
    
     button.addEventListener('click', function() {
-         alert(name.textContent + ": " + input.value);
+         alert("Kelly Tan: " + input.value);
          output.innerHTML = input.value;
          
     });
